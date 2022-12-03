a9abdd4b34ce78f9fead0ed8cd3dd86779ecf629
Intrstng <Intrstng@gmail.com>
2022-12-03 15:25:03 +0300

    init: task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+  let newArr = n.toString().split('').reverse();
+
+  if (newArr[newArr.length - 1] === '-') newArr.pop();
+  return parseInt(newArr.join(''));
 }
